1430844f48767efec053351594a31d9a975b74e0
tswic-highdesert <97049574+tswic-highdesert@users.noreply.github.com>
2024-03-02 13:40:19 -0600

    Tweaks

--- eclipseJam/Levels/ArenaManager.gd
+++ eclipseJam/Levels/ArenaManager.gd
@@ -2,19 +2,46 @@ extends Node2D
 
 
 #Enemy Types
-# Cyclops - Melee
-# Ghost - Melee
+# Ghost - Melee (easiest)
 # Harpee - Ranged
-# Medusa - Ranged
+# Cyclops - Melee
 # Minotaur - AOE
+# Medusa - Ranged (hardest)
+
+const CYCLOPS = preload("res://Enemies/cyclops.tscn")
+const GHOST = preload("res://Enemies/ghost.tscn")
+const HARPEE = preload("res://Enemies/harpee.tscn")
+const MEDUSA = preload("res://Enemies/medusa.tscn")
+const MINOTAUR = preload("res://Enemies/minotaur.tscn")
+
+var enemiesWave1 = [
+	GHOST
+]
+
+var enemiesWave2 = [
+	GHOST,
+	HARPEE
+]
+
+var enemiesWave3 = [
+	GHOST,
+	HARPEE,
+	CYCLOPS
+]
+
+var enemiesWave4 = [
+	GHOST,
+	HARPEE,
+	CYCLOPS,
+	MINOTAUR
+]
+
+var enemiesWave5 = [
+	GHOST,
+	HARPEE,
+	CYCLOPS,
+	MINOTAUR,
+	MEDUSA
+]
 
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass

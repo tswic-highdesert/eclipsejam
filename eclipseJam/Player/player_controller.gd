bb00d584a32e7937c0e053744d529817d253c68a
FrostyShock <ChrisMBerry@outlook.com>
2024-03-05 17:00:35 -0600

    Weapon Switching and Animations

--- eclipseJam/Player/player_controller.gd
+++ eclipseJam/Player/player_controller.gd
@@ -26,6 +26,7 @@ func _physics_process(delta):
 	move_and_collide(velocity)
 	calc_anims()
 	look_rotation()
+	
 
 func calc_movement(delta):
 	var direction = Vector2(
@@ -48,9 +49,17 @@ func flip():
 	var direction = sign(get_global_mouse_position().x - $Player.global_position.x)
 	if direction < 0:
 		$Player.set_flip_h(true)
+		$Player/Unarmed.set_flip_v(true)
+		$Player/Gun.set_flip_v(true)
+		$Player/Staff.set_flip_v(true)
+		
 	else:
 		$Player.set_flip_h(false)
-
+		$Player/Unarmed.set_flip_v(false)
+		$Player/Gun.set_flip_v(false)
+		$Player/Staff.set_flip_v(false)
+		
+		
 func spawn_ground_effects():
 	Global.instance_scene_on_main(DUST, $GroundMarker.global_position)
 	SoundFX.play("grass_step")
@@ -59,12 +68,18 @@ func look_rotation():
 	#Gets the mouse location and sets the muzzle rotation to match
 	var look_vector = get_global_mouse_position() - global_position
 	weapon1.global_rotation = atan2(look_vector.y, look_vector.x)
+	weapon2.global_rotation = atan2(look_vector.y, look_vector.x)
+	weapon3.global_rotation = atan2(look_vector.y, look_vector.x)
 
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			if currentWeapon == 2:
+			if currentWeapon == 1:
+				melee_attack_unarmed()
+			elif currentWeapon == 2:
 				fire_bullet()
+			elif currentWeapon == 3:
+				melee_attack_staff()
 				
 	elif InputEventKey:
 		if event.is_action_pressed("slot_one"):
@@ -83,10 +98,9 @@ func _input(event):
 func switch_weapon(newWeapon):
 	if newWeapon == 1:
 		weapon1.visible = true
-		$Player/Unarmed/UnarmedCollision.disabled = false
+		$Player/Unarmed/UnarmedCollision.disabled = true
 		weapon2.visible = false
 		weapon3.visible = false
-		$Player/Staff/StaffCollision.disabled = true
 		currentWeapon = 1
 	elif newWeapon == 2:
 		weapon1.visible = false
@@ -100,13 +114,44 @@ func switch_weapon(newWeapon):
 		$Player/Unarmed/UnarmedCollision.disabled = false
 		weapon2.visible = false
 		weapon3.visible = true
-		$Player/Staff/StaffCollision.disabled = false
 		currentWeapon = 3
 	pass
 
-func melee_attack():
-	
-	pass
+func melee_attack_unarmed():
+	if weapon1.visible == true: 
+		$Player/Unarmed/UnarmedCollision.disabled = false
+		weapon1.scale = Vector2(0.8, 1.2)
+		await get_tree().create_timer(0.1).timeout  # Delay 1: 0.1 seconds
+		weapon1.scale = Vector2(1.2, 0.8)  # Second scale
+		await get_tree().create_timer(.2).timeout  # Delay for 0.3 seconds
+		weapon1.scale = Vector2(1.0, 1.0)
+		await get_tree().create_timer(0.3).timeout  # Delay 2: 0.2 seconds
+		weapon1.scale = Vector2(0.9, 1.1)  # Reset scale after the delay
+		$Player/Unarmed/UnarmedCollision.disabled = true
+	else:
+		pass
+
+func melee_attack_staff():
+	if weapon3.visible == true:
+		var direction = sign(get_global_mouse_position().x - $Player.global_position.x)
+		$Player/Staff/StaffCollision.disabled = false
+		var initialRotation = null
+		var targetRotation = null
+		if direction < 0:
+			initialRotation = weapon3.rotation +20
+			targetRotation = initialRotation -2
+		else:
+			initialRotation = weapon3.rotation -20
+			targetRotation = initialRotation + 2
+		var duration = .5
+		var elapsed = 0.0
+		while elapsed < duration:
+			weapon3.rotation = lerp(initialRotation, targetRotation, elapsed / duration)
+			await get_tree().process_frame
+			elapsed += get_process_delta_time()
+		$Player/Staff/StaffCollision.disabled = true
+	else:
+		pass
 
 func fire_bullet():
 	if (canFire == true):

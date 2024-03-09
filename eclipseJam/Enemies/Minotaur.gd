e1cb8d1f4846f85c635474fd73f88cc86ec2452f
FrostyShock <ChrisMBerry@outlook.com>
2024-03-08 20:49:00 -0600

    working

--- eclipseJam/Enemies/Minotaur.gd
+++ eclipseJam/Enemies/Minotaur.gd
@@ -9,10 +9,12 @@ var chargeSpeed = 2000
 var motion = Vector2.ZERO
 var currentSpeed = normalSpeed
 var inRange = false
+var isCharging = false
 
 
 func _physics_process(delta):
-		chase_player(delta)
+	chase_player(delta)
+	print(currentSpeed)
 		
 
 func chase_player(delta):
@@ -28,13 +30,14 @@ func flip():
 
 func _on_aggro_radius_body_entered(body):
 	currentSpeed = aggroSpeed
-	#SignalManager.enemy_destroyed.emit(self)
-	#queue_free()
+	
+	
 
 func _on_hurtbox_hit(damage):
 	stats.health - damage
 	
 func charge_player():
+	$ChargeCooldown.start();
 	currentSpeed = chargeSpeed
 	var playerRelativePos = Global.player.global_position - global_position
 	var offset = playerRelativePos.normalized() * Vector2(500, 500)  # Adjust the offset as needed
@@ -45,28 +48,35 @@ func charge_player():
 		#pass
 	
 func _on_charge_range_body_entered(body):
-	print(Global.player.global_position)
-	print(position)
-	inRange == true
-	currentSpeed = 0
-	$ChargeCooldown.start();
-	$RunToTimer.start();
-	$aggroRadius.monitoring = false
-	$aggroRadius/CollisionShape2D.disabled = true
-	$ChargeRange.monitoring = false
-	$ChargeRange/ChargeTrigger.disabled = true
+	if !isCharging:
+		print(Global.player.global_position)
+		print(position)
+		inRange = true
+		
+		currentSpeed = 0
+		
+		$RunToTimer.start();
+		#$aggroRadius.monitoring = false
+		#$aggroRadius/CollisionShape2D.disabled = true
+		#$ChargeRange.monitoring = false
+		#$ChargeRange/ChargeTrigger.disabled = true
 	
 func _on_charge_cooldown_timeout():
-	inRange = false
-	$aggroRadius.monitoring = true
-	$aggroRadius/CollisionShape2D.disabled = false
-	$ChargeRange.monitoring = true
-	$ChargeRange/ChargeTrigger.disabled = false
-	currentSpeed = normalSpeed
-	pass # Replace with function body.
+	if currentSpeed == chargeSpeed:
+		currentSpeed = 0
+		isCharging = false
+		$ChargeCooldown.start()
+	elif currentSpeed == 0:
+		isCharging = false
+		currentSpeed = normalSpeed
+	#$aggroRadius.monitoring = true
+	#$aggroRadius/CollisionShape2D.disabled = false
+	#$ChargeRange.monitoring = true
+	#$ChargeRange/ChargeTrigger.disabled = false
 
 
 func _on_run_to_timer_timeout():
 	print(Global.player.global_position)
 	charge_player()
+	isCharging = true
 	pass # Replace with function body.
